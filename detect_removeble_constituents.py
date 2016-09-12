745f5cfab5cf74b7bc90343e4844e957e89bebb7
Devon Fritz <devon.s.fritz@gmail.com>
2016-09-12 19:55:21 +0200

    removed poss and removed brackets.

--- detect_removeble_constituents.py
+++ detect_removeble_constituents.py
@@ -6,6 +6,10 @@ import datetime
 import dill
 import argparse
 from multiprocessing import Process
+import matplotlib.pyplot as plt
+plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
 EXCLUDED_POS = ['NNP', 'NN', 'VBP', 'VBD', '.', 'CC', 'DT', 'NNS', 'IN', 'PRP',
                 'TO', 'VB', 'PRP$', 'VBG', ',', 'VBN', 'VBZ', 'WP']
@@ -18,7 +22,8 @@ REMOVABLE_TYPES = ['advmod', # includes quantmod
                    # 'nummod', # Same ate [3] sheep [number and num
                    # usually needed
                    #'quantmod', # included in 'advmod'
-                   'nmod:poss', # Bill's clothes ['poss']
+                   #'nmod:poss', # Bill's clothes ['poss'] - lots of u
+                   # ungrammatical results
                    # 'det:predet', # All the boys are here ['predet'] too few
                    # 'case', # prepositions - too many and most were needed
                    # TODO DSF - mark the whole prepositional phrase maybe?
@@ -30,6 +35,45 @@ REMOVABLE_TYPES = ['advmod', # includes quantmod
                    ]
 DELIMITER = '|'
 TOKENS_WITHOUT_SPACE = ["'", ".", "!", ":", ",", "n't"]
+
+
+def filter_out_mods(mods):
+    for m in mods:
+        mod = m[2]
+        pos = mod[1]
+        modtype = m[1]
+        word = mod[0]
+
+        if modtype == 'nmod:poss' and pos == 'PRP$':
+            continue
+
+        if word.lower() == 'when':
+            continue
+
+        yield m
+
+def is_node_relevant(n):
+    if n['rel'] not in REMOVABLE_TYPES:
+        return False
+    if n['word'].lower() == 'when':
+        return False
+
+    return True
+
+def plot_frequencies(data, ax, legend_label):
+    # now sort tuples by count
+    data = sorted(data, key=lambda tup: tup[1], reverse=True)
+
+    # tuples (pair,count)
+    x = [t[0] for t in data]
+    y = [t[1]  for t in data]
+
+    x_pos = np.arange(len(x))
+
+    ax.plot(x_pos, y, label=legend_label)
+    ax.set_yscale('log')
+    plt.xlim([0, len(x)])
+    plt.ylim([.5, y[0]])
 
 
 def get_statistics_for_sentence(prev_stats, interesting_records):
@@ -97,8 +141,10 @@ def write_line(story, nodes, trigger_node, words_to_remove, output_file,
 
     # Go though each word of the sentence and see if it should be
     # removed.
+    '''
     compiled_sentence = ''
     for i, n in nodes.items():
+        print(n)
         if i == 0:
             continue
 
@@ -118,6 +164,12 @@ def write_line(story, nodes, trigger_node, words_to_remove, output_file,
                 compiled_sentence += ']'
 
     output_file.write(DELIMITER + compiled_sentence)
+    '''
+    # Output the full sentence
+    output_file.write(DELIMITER +
+                      story.original_sentences[sentence_num] \
+                      .replace('\r','').replace('\n', ''))
+
 
     # Output the interesting words
     output_file.write(DELIMITER + trigger_node['rel'])
@@ -131,11 +183,6 @@ def write_line(story, nodes, trigger_node, words_to_remove, output_file,
 
     # head word index
     output_file.write(DELIMITER + str(trigger_node['head']))
-    # Output the full sentence
-    output_file.write(DELIMITER +
-                      story.original_sentences[sentence_num] \
-                      .replace('\r','').replace('\n', ''))
-
     output_file.write('\r\n')
 
 
@@ -156,12 +203,15 @@ def handle_sentence(story, story_filename, sentence_num, output_file, stats):
 
     # Triple format
     # [(head, POS), dep-type, (mod, POS)]
+    # [(('walk', 'VB'), 'advmod', ('now', 'RB'))]
     # this is always a list, but we always just want the first element
     dsent = dsent[0]
 
     interesting_records = list(x for x in p[0] if x[1] in REMOVABLE_TYPES)
-    count = len(interesting_records)
+    # apply some filtering of the records
+    interesting_records = list(filter_out_mods(interesting_records))
 
+    count = len(interesting_records)
     # Get statistics based on the sentence
     results = get_statistics_for_sentence(stats, interesting_records)
 
@@ -169,25 +219,30 @@ def handle_sentence(story, story_filename, sentence_num, output_file, stats):
     if count == 0:
         return results
 
-    t_sent = word_tokenize(sent)
-
     # INTERESTING SENTENCE FOUND - let's write it out to the file
+    words_with_apos = 0
+    words_seen = -1
     for i, n in dsent.nodes.items():
+        words_seen += 1
         if i == 0:
             continue
 
+        if "'" in n['word']:
+            words_with_apos += 1
+
         words_to_remove = []
         # check to see if the node has any of the phenomena we care about
-        if n['rel'] in REMOVABLE_TYPES:
+        if is_node_relevant(n):
             # This is the occurrence we should consider,
             # so do the work
-            words_to_remove.append(i)
+            words_to_remove.append(words_seen - words_with_apos)
 
             # also remove any dependent nodes
             remove_dependent_nodes(dsent.nodes, i, words_to_remove)
 
             write_line(story, dsent.nodes, n, words_to_remove, output_file,
                        story_filename, sentence_num)
+
 
 def output_stats(output_path, stats):
     if not os.path.isdir(output_path):
@@ -214,6 +269,8 @@ def output_stats(output_path, stats):
             for k,v in v['pos'].items():
                 f.write('\t\t' + k + ': ' + str(v) + '\r\n')
 
+    fig, ax = plt.subplots()
+
     with open(output_path + 'heads_and_modifiers.csv', 'w') as f:
         # try to bring everything together and sort it
 
@@ -230,20 +287,45 @@ def output_stats(output_path, stats):
                 f.write(k + DELIMITER + k2 + DELIMITER +
                         str(v2) + DELIMITER + str(total) +
                         DELIMITER + str(v2/total)+'\r\n')
+                all_items.append((k + k2, v2))
+
+        start_time = time.time()
+
+        plot_frequencies(all_items, ax, 'Modifier/Head')
+
+        end_time = time.time()
+        full_time = end_time - start_time
+        print('Time at the end: ' + str(datetime.timedelta(seconds=full_time)))
+
+
 
     with open(output_path + 'heads.csv', 'w') as f:
+        all_heads = []
         # header
         f.write('head' + DELIMITER + 'count\r\n')
 
         for k,v in stats['heads'].items():
             f.write(k + DELIMITER + str(v) + '\r\n')
+            all_heads.append((k, v))
+
+        plot_frequencies(all_heads, ax, 'Head')
 
     with open(output_path + 'modifiers.csv', 'w') as f:
+        all_mods = []
         # header
         f.write('modifier' + DELIMITER + 'count\r\n')
 
         for k,v in stats['modifiers'].items():
-            f.write(k + DELIMITER + str(sum(v.values())) + '\r\n')
+            s = sum(v.values())
+            f.write(k + DELIMITER + str(s) + '\r\n')
+            all_mods.append((k, s))
+
+        plot_frequencies(all_mods, ax, 'Modifier')
+
+
+    plt.legend()
+    plt.savefig(output_path + '/freq_dist.png')
+
 
 def finish_up(stats):
     output_stats('../stats/', stats)
@@ -266,17 +348,16 @@ def do_loop(file_list, process_num, output_file):
         o.write('Source' + DELIMITER +
                           'StoryID' + DELIMITER +
                           'File' + DELIMITER +
-                          'Full Story' + DELIMITER +
+                  'Full Story' + DELIMITER +
                           'Title' + DELIMITER +
                           'Sentence Number' + DELIMITER +
-                          'Marked Sentence' + DELIMITER +
+                          'Original Sentence' + DELIMITER +
                           'Modifier Type' + DELIMITER +
                           'Modifier' + DELIMITER +
                           'Head' + DELIMITER +
                           'Removed Words Start Index' + DELIMITER +
                           'Removed Words End Index' + DELIMITER +
-                          'Head Word Index' + DELIMITER +
-                          'Original Sentence'+
+                          'Head Word Index'+
                           '\r\n')
 
         # go through each file and process the information we care about
